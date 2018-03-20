ea5b13e6be65dfc621232fad2b0d6bc6a2cd0eb1
Ron Belmont <belmonrj@gmail.com>
2018-03-20 00:32:21 -0400

    Reduced number of various bins to make life easier

--- RpPar.h
+++ RpPar.h
@@ -35,7 +35,8 @@ static const int NDET = 34; // number of detectors (see below)
 // 31 -- FVTXS clusters 0 1 3
 // 32 -- FVTXS clusters 0 2 3
 // 33 -- FVTXS clusters 1 2 3
-static const int NDETSHORT = 9; // number of detectors (see below)
+static const int NDETSHORT = 1; // number of detectors (see below)
+//static const int NDETSHORT = 9; // number of detectors (see below)
 //  0 -- BBCS Psi2 calculated
 //  1 -- FVTXS Psi2 calculated
 //  2 -- FVTXN Psi2 calculated
@@ -45,8 +46,8 @@ static const int NDETSHORT = 9; // number of detectors (see below)
 //  6 -- FVTXS layer 3 Psi2 calculated (with weight)
 //  7 -- FVTXS [-1, -2] Psi2 calculated
 //  8 -- FVTXS [-2, -3] Psi2 calculated
-static const int NHAR = 4; // number of harmonics (1, 2, 3)
-static const int NMUL = 6; // centrality bins 0-5, 5-10, 10-20, 20-40, 40-60, 60-88
+static const int NHAR = 2; // number of harmonics (1, 2, 3)
+static const int NMUL = 1; // centrality bins 0-5, 5-10, 10-20, 20-40, 40-60, 60-88
 static const int NZPS = 10; // number of z-vertex bins for flattening (very important for FVTX, maybe not so important for BBC)
 static const int NORD = 12; // number of orders for fourier fit of Psi2 distribution (sin and cos?)
 // --- all below here are irrelevant for now?
