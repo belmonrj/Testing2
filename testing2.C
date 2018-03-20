2cea5b71cd851e5603ed6a114456f19b5f33b8ae
Ron Belmont <belmonrj@gmail.com>
2018-03-19 22:52:12 -0400

    Added outputfile and a histogram, and a bunch of the variables

--- testing2.C
+++ testing2.C
@@ -55,8 +55,8 @@ int main(int argc, char *argv[])
   cout << "Now processing with run number " << run << endl;
 
   flatten(run, 1);
-  flatten(run, 2);
-  flatten(run, 3);
+  // flatten(run, 2);
+  // flatten(run, 3);
 
   return 0;
 
@@ -67,6 +67,22 @@ void flatten(int runnumber, int passnumber)
 {
 
   cout << "run number = " << runnumber << " pass number = " << passnumber << endl;
+
+
+
+  char outfile1[300];
+  //sprintf(outfile1, "%s%s%d%s%d%s", "output/files_", species.Data(), energyflag, "/hist_", runNumber, ".root");
+  sprintf(outfile1, "output/files_heau200/hist_%d.root", runnumber);
+  cout << "histogram output file: " << outfile1 << endl;
+
+  TFile *mData1 = TFile::Open(outfile1, "recreate");
+  mData1->cd();
+
+  TH1D* heta = new TH1D("heta","",32,-3.2,3.2);
+
+  //------------------------------------------------------------//
+  //  Finished initializing histograms                          //
+  //------------------------------------------------------------//
 
   // --- get the number of files for this run number
   //string pipe_out = (string) gSystem->GetFromPipe(Form("ls input_%s/%d_*.root | grep -c r", species.Data(), runNumber));
@@ -92,9 +108,51 @@ void flatten(int runnumber, int passnumber)
   cout << "Will attempt to process " << nevents << " events for this run" << endl;
   for ( int i = 0; i < nevents; ++i ) // loop over events
     {
+      if ( i > 1000 ) break;
+
+      // --- get this event
       ktree->GetEntry(i);
-      int centrality = ktree->centrality;
-      //cout << "centrality for this event is " << centrality << endl;
-    }
+
+      // --- get some event variables
+      Float_t event = ktree->event;
+      Float_t bbc_z = ktree->bbc_z;
+      Float_t centrality = ktree->centrality;
+      Int_t   npc1 = ktree->npc1;
+      UInt_t  trigger_scaled = ktree->trigger_scaled;
+      UInt_t  trigger_live = ktree->trigger_live;
+      Float_t bc_x = ktree->bc_x;
+      Float_t bc_y = ktree->bc_y;
+      Float_t vtx_z = ktree->vtx_z;
+      Float_t fvtx_x = ktree->fvtx_x;
+      Float_t fvtx_y = ktree->fvtx_y;
+      Float_t fvtx_z = ktree->fvtx_z;
+      Float_t frac = ktree->frac;
+      Float_t bbc_qn = ktree->bbc_qn;
+      Float_t bbc_qs = ktree->bbc_qs;
+
+      // --- do a loop over fvtx tracks
+      int nfvtxt = ktree->ntracklets;
+      for ( int i = 0; i < nfvtxt; ++i )
+        {
+          float eta = ktree->feta[i];
+          float the = ktree->fthe[i];
+          float phi = ktree->fphi[i];
+          float ch2ndf = ktree->fchisq[i];
+          int arm = ktree->farm[i];
+          int nhist = ktree->fnhits[i];
+          float dcax = ktree->fDCA_X[i];
+          float dcay = ktree->fDCA_Y[i];
+          heta->Fill(eta);
+        } // end loop over fvtx tracks
+
+    } // end loop over events
+
+  cout << "Finished processing events" << endl;
+  cout << "Now attempting to write output file " << outfile1 << " at memory address " << mData1 << endl;
+
+  mData1->Write();
+  mData1->Close();
+
+  cout << "All done.  Prepare for unforeseen consequences." << endl;
 
 }
