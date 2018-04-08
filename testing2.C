4f274851af12f2f6b1b144f0970cc06a67b66372
Ron Belmont <belmonrj@gmail.com>
2018-04-08 16:40:32 -0400

    Changed nhits from 4 to 3, still requiring no VTX, 9 of 133 missing from output

--- testing2.C
+++ testing2.C
@@ -666,7 +666,7 @@ void flatten(int runnumber, int passnumber)
           if ( chi2ndf < 0 || chi2ndf > 5 ) continue;
           if ( fabs(dcax) > 2 || fabs(dcay) > 2 ) continue;
           //if ( nhits < 4 ) continue;
-          if ( nhitx < 4 ) continue;
+          if ( nhitx < 3 ) continue;
           if ( nhitx != nhits ) continue; // require exactly 0 VTX hits
           // --- fill some diagnostic histos after cuts
           brheta->Fill(eta);
