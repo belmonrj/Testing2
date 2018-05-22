caadacb4910cd5c340c82e0cb9aafd38d10563cd
Ron Belmont <belmonrj@gmail.com>
2018-05-22 10:24:40 +0200

    Additional functions for comp1

--- CheckProd/comp1.C
+++ CheckProd/comp1.C
@@ -1,3 +1,6 @@
+TGraphErrors* get_systematics(TH1D*);
+TGraphErrors* get_systematics(TH1D*, const double);
+
 void comp1()
 {
 
@@ -41,7 +44,72 @@ void comp1()
   latt.DrawLatex(0.56, 0.87, "^{3}He+Au #sqrt{s_{_{NN}}} = 200 GeV");
   latt.DrawLatex(0.56, 0.80, "EP: -3.9 < #eta < -3.1");
 
-  c1->Print("Figures/COMPROD_v2eta_clean_heau200.png");
-  c1->Print("Figures/COMPROD_v2eta_clean_heau200.pdf");
+  c1->Print("Figures/COMPROD_v2eta_final_heau200.png");
+  c1->Print("Figures/COMPROD_v2eta_final_heau200.pdf");
 
+  TH1D* th1d_v2eta = (TH1D*)histo1->Clone("th1d_v2eta");
+  for ( int i = 0; i < 32; ++i )
+    {
+      double eta = th1d_v2eta->GetBinCenter(i+1);
+      if ( eta > -1 && eta < 1 )
+        {
+          th1d_v2eta->SetBinContent(i+1,histo2->GetBinContent(i+1));
+          th1d_v2eta->SetBinError(i+1,histo2->GetBinError(i+1));
+        }
+    }
+
+  TGraphErrors* tge_sys = get_systematics(th1d_v2eta);
+  tge_sys->SetFillColorAlpha(kBlack,0.35);
+
+  hdummy->Draw();
+  th1d_v2eta->Draw("same ex0p");
+  tge_sys->Draw("2");
+
+  latt.DrawLatex(0.30, 0.87, "PHENIX");
+  latt.DrawLatex(0.56, 0.87, "^{3}He+Au #sqrt{s_{_{NN}}} = 200 GeV");
+  latt.DrawLatex(0.56, 0.80, "EP: -3.9 < #eta < -3.1");
+
+  c1->Print("Figures/NEW_v2eta_final_heau200.png");
+  c1->Print("Figures/NEW_v2eta_final_heau200.pdf");
+
+  TFile* outfile = TFile::Open("heau200_v2eta_new.root","recreate");
+  outfile->cd();
+  tge_sys->SetName("tge_v2eta_sys");
+  tge_sys->Write();
+  th1d_v2eta->Write();
+  outfile->Close();
+
+}
+
+TGraphErrors* get_systematics(TH1D* h)
+{
+  return get_systematics(h,0.1);
+}
+
+TGraphErrors* get_systematics(TH1D* h, const double sys)
+{
+  const int nbins = h->GetNbinsX();
+  vector<double> v_eta;
+  vector<double> v_v2;
+  for ( int i = 0; i < nbins; ++i )
+    {
+      double eta = h->GetBinCenter(i+1);
+      double v2 = h->GetBinContent(i+1);
+      if ( v2 > 0 )
+        {
+          v_eta.push_back(eta);
+          v_v2.push_back(v2);
+        }
+    }
+  const int size = v_eta.size();
+  double x[size], y[size], ex[size], ey[size];
+  for ( int i = 0; i < size; ++i )
+    {
+      x[i] = v_eta[i];
+      y[i] = v_v2[i];
+      ex[i] = 0.08;
+      ey[i] = sys*v_v2[i];
+    }
+  TGraphErrors* tge = new TGraphErrors(size,x,y,ex,ey);
+  return tge;
 }
