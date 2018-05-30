f3817cbd5fc30a75eab5034ecc6226d346096d7e
Ron Belmont <belmonrj@gmail.com>
2018-05-29 21:07:03 -0400

    Set beam offset to zero

--- testing2.C
+++ testing2.C
@@ -89,9 +89,11 @@ void flatten(int runnumber, int passnumber)
     // beam_y = 0.06;
     // beam_angle = -0.001;
     // --- numbers from Darren and Sanghoon, updated pro build
-    beam_x = 0.3806;
-    beam_y = 0.0131;
+    //beam_x = 0.3806;
+    //beam_y = 0.0131;
     //beam_angle = 0.0018; // positive angle?
+    beam_x = 0.0;
+    beam_y = 0.0;
     beam_angle = 0.0; // testing to compare to Shengli
     // --- try 0
     // beam_x = 0.0;
